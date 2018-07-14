4a74b67f345b0bc8b1ced0510c52fa3770c24588
RajatRK <RajatRK@DESKTOP-AR5SSDK>
2018-07-14 20:23:58 +0530

    ComplainDAO

---
+++ Anti Ragging/src/model/ComplainDAO.java
@@ -0,0 +1,37 @@
+package model;
+
+import org.hibernate.Query;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.classic.Session;
+
+public class ComplainDAO {
+	public Session getSession() {
+		AnnotationConfiguration con = new AnnotationConfiguration();
+		con.configure("hibernate.cfg.xml");
+		Session session = con.buildSessionFactory().openSession();
+		return session;
+	}
+	
+	public void addComplain(Complain complain) {
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		session.persist(complain);
+		transaction.commit();
+		session.close();
+	}
+	
+	public Complain getComplainById(int id) {
+		Session session = getSession();
+		Complain comp = (Complain) session.get(Complain.class, id);
+		session.close();
+		return comp;
+	}
+	
+	public void getAllComplains() {
+		Session session = getSession();
+		Query q = session.createQuery("FROM complain");
+			session.close();
+	}
+}
+
